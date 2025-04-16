0f1f395648a3f364162b0a7496a30132f0e0aeae
paulinaromerosdl <promeros@nd.edu>
2025-04-16 17:23:56 -0400

    adding a title to the app and a description of what the app does

--- NERStreamlitApp/ner_app.py
+++ NERStreamlitApp/ner_app.py
@@ -3,3 +3,5 @@ import spacy
 from spacy.pipeline import EntityRuler
 from spacy import displacy 
 nlp= spacy.load("en_core_web_sm")
+st.title("Custom Named Entity Recognition (NER) App")
+st.markdown("This app lets you input or upload text and define entities using spaCy's 'EntityRuler' Paste your custom patterns below, and explore the extracted entities!")
