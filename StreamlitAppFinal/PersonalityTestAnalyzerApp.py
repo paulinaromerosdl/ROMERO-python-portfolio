ee0e90cccfa375575ca5e5cff543e22c200cd81d
paulinaromerosdl <promeros@nd.edu>
2025-05-07 21:30:24 -0400

    setting up titles

--- StreamlitAppFinal/PersonalityTestAnalyzerApp.py
+++ StreamlitAppFinal/PersonalityTestAnalyzerApp.py
@@ -3,3 +3,6 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+#Setting up app
+st.title("🧠Personality Test Analyzer")
+st.markdown("Upload your MBTI or Big Five personality test results to see visual insights")
