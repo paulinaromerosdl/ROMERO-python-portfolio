3152324824dff7ba8900ee71212dbba6e8efea53
paulinaromerosdl <promeros@nd.edu>
2025-05-07 22:06:36 -0400

    setting up app title and instructions

--- StreamlitAppFinal/PersonalityTestAnalyzerApp.py
+++ StreamlitAppFinal/PersonalityTestAnalyzerApp.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-#Setting up app
-st.title("🧠Personality Test Analyzer")
-st.markdown("Upload your MBTI or Big Five personality test results to see visual insights")
+
+# App Title and description
+st.title("🧠 Big Five Personality Test Analyzer")
+st.markdown("This app is dedicated to analyze your results from the Big Five Personality Test invented by Ernest Tupes and Raymond Christal. Upload your results in a csv file or input the data manually below!")
